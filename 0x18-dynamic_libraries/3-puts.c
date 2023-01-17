4d7be409a072a5a94e327581a0a152cddf400468
AmareGatie <amaregatie2006@gmail.com>
2023-01-17 01:47:08 -0600

    task complates

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
-  * _puts - Prints a string
-  * @str: The string to print
-  *
-  * Return: void
-  */
+ * _puts - Prints a string to stdout.
+ * @str: The string to be printed.
+ */
 void _puts(char *str)
 {
-	for (; *str != '\0'; str++)
-	{
-		_putchar(*str);
-	}
+	int c = 0;
 
+	while (str[c] != 0)
+	{
+		_putchar(str[c]);
+		c++;
+	}
 	_putchar('\n');
 }
